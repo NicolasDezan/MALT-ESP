d394975ca003e45aba3e0b0dde72cb6b943c0b25
Nicolas Dezan <nicolinugfx@gmail.com>
2025-02-22 11:07:25 -0300

    [Parameters] Consolidação de ParametersSections.kt para gerenciar os componentes da tela de Parameters

---
+++ app/src/main/java/com/nicolas/maltesp/ui/scaffold/bottombar/content/parameters/ParametersSections.kt
@@ -0,0 +1,139 @@
+package com.nicolas.maltesp.ui.scaffold.bottombar.content.parameters
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.nicolas.maltesp.others.dataclasses.ParameterData
+import com.nicolas.maltesp.others.dataclasses.ParameterGroup
+import com.nicolas.maltesp.others.dataclasses.ParameterSectionData
+import com.nicolas.maltesp.viewmodels.BluetoothViewModel
+import com.nicolas.maltesp.viewmodels.ParametersViewModel
+
+@Composable
+fun parameterSectionData(parametersViewModel: ParametersViewModel, bluetoothViewModel: BluetoothViewModel): List<ParameterSectionData> {
+    val parametersState by parametersViewModel.parametersState.collectAsState()
+    val parametersRange by parametersViewModel.parametersRange.collectAsState()
+    val parametersReceived by bluetoothViewModel.parametersReceived.collectAsState()
+    val isBluetoothConnected = bluetoothViewModel.isConnected()
+
+    val sections = listOf(
+        ParameterSectionData(
+            title = "Maceração",
+            groups = listOf(
+                ParameterGroup(
+                    parameters = listOf(
+                        ParameterData(
+                            name = "Tempo Submerso",
+                            unit = "h",
+                            description = "",
+                            state = parametersState.steeping.submergedTime,
+                            range = parametersRange.steeping.submergedTime,
+                            isEquals = parametersViewModel.compareSteepingSubmergedTime(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isSteepingSubmergedTimeValid()
+                        ),
+                        ParameterData(
+                            name = "Volume de Água",
+                            unit = "mL",
+                            description = "",
+                            state = parametersState.steeping.waterVolume,
+                            range = parametersRange.steeping.waterVolume,
+                            isEquals = parametersViewModel.compareSteepingWaterVolume(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isSteepingWaterVolumeValid()
+                        ),
+                        ParameterData(
+                            name = "Tempo Descanso",
+                            unit = "h",
+                            description = "",
+                            state = parametersState.steeping.restTime,
+                            range = parametersRange.steeping.restTime,
+                            isEquals = parametersViewModel.compareSteepingRestTime(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isSteepingRestTimeValid()
+                        ),
+                        ParameterData(
+                            name = "Número de Ciclos",
+                            unit = "",
+                            description = "",
+                            state = parametersState.steeping.cycles,
+                            range = parametersRange.steeping.cycles,
+                            isEquals = parametersViewModel.compareSteepingCycles(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isSteepingCyclesValid()
+                        )
+                    )
+                )
+            )
+        ),
+        ParameterSectionData(
+            title = "Germinação",
+            groups = listOf(
+                ParameterGroup(
+                    parameters = listOf(
+                        ParameterData(
+                            name = "Nível de Rotação",
+                            unit = "",
+                            description = "",
+                            state = parametersState.germination.rotationLevel,
+                            range = parametersRange.germination.rotationLevel,
+                            isEquals = parametersViewModel.compareGerminationRotationLevel(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isGerminationRotationLevelValid()
+                        ),
+                        ParameterData(
+                            name = "Tempo Total",
+                            unit = "h",
+                            description = "",
+                            state = parametersState.germination.totalTime,
+                            range = parametersRange.germination.totalTime,
+                            isEquals = parametersViewModel.compareGerminationTotalTime(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isGerminationTotalTimeValid()
+                        ),
+                        ParameterData(
+                            name = "Volume de Água",
+                            unit = "mL",
+                            description = "",
+                            state = parametersState.germination.waterVolume,
+                            range = parametersRange.germination.waterVolume,
+                            isEquals = parametersViewModel.compareGerminationWaterVolume(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isGerminationWaterVolumeValid()
+                        ),
+                        ParameterData(
+                            name = "Adição de Água",
+                            unit = "min",
+                            description = "",
+                            state = parametersState.germination.waterAddition,
+                            range = parametersRange.germination.waterAddition,
+                            isEquals = parametersViewModel.compareGerminationWaterAddition(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isGerminationWaterAdditionValid()
+                        )
+                    )
+                )
+            )
+        ),
+        ParameterSectionData(
+            title = "Secagem",
+            groups = listOf(
+                ParameterGroup(
+                    parameters = listOf(
+                        ParameterData(
+                            name = "Temperatura",
+                            unit = "°C",
+                            description = "",
+                            state = parametersState.kilning.temperature,
+                            range = parametersRange.kilning.temperature,
+                            isEquals = parametersViewModel.compareKilningTemperature(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isKilningTemperatureValid()
+                        ),
+                        ParameterData(
+                            name = "Tempo",
+                            unit = "min",
+                            description = "",
+                            state = parametersState.kilning.time,
+                            range = parametersRange.kilning.time,
+                            isEquals = parametersViewModel.compareKilningTime(parametersReceived, isBluetoothConnected),
+                            isNumberValid = parametersViewModel.isKilningTimeValid()
+                        )
+                    )
+                )
+            )
+        )
+    )
+    return sections
+}
