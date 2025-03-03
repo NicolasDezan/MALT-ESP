45b36dc2e6722649e74e024bae080140859eb1ea
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-03 17:50:52 -0300

    [Parameters] Criação da função para tratar os dados Bluetooth - recebe uma lista de Int e retorna um ParametersState

--- app/src/main/java/com/nicolas/maltesp/others/classes/Parameters.kt
+++ app/src/main/java/com/nicolas/maltesp/others/classes/Parameters.kt
@@ -36,23 +36,53 @@ class Parameters {
             )
         }
 
-        fun testParametersState(): ParametersState {
+        fun receiveParameters(convertedData: List<Int>): ParametersState {
             return ParametersState(
                 steeping = SteepingState(
-                    submergedTime = mutableStateOf(10.toString()),
-                    waterVolume = mutableStateOf(500.toString()),
-                    restTime = mutableStateOf(4.toString()),
-                    cycles = mutableStateOf(4.toString())
+                    submergedTime = mutableStateOf(
+                        (convertedData[1] * MultiplierRange.Steeping.SUBMERGED_TIME + MinRangeValues.Steeping.SUBMERGED_TIME)
+                            .toString()
+                    ),
+                    waterVolume = mutableStateOf(
+                        (convertedData[2] * MultiplierRange.Steeping.WATER_VOLUME + MinRangeValues.Steeping.WATER_VOLUME)
+                            .toString()
+                    ),
+                    restTime = mutableStateOf(
+                        (convertedData[3] * MultiplierRange.Steeping.REST_TIME + MinRangeValues.Steeping.REST_TIME)
+                            .toString()
+                    ),
+                    cycles = mutableStateOf(
+                        (convertedData[4] * MultiplierRange.Steeping.CYCLES + MinRangeValues.Steeping.CYCLES)
+                            .toString()
+                    )
                 ),
                 germination = GerminationState(
-                    rotationLevel = mutableStateOf(10.toString()),
-                    totalTime = mutableStateOf(78.toString()),
-                    waterVolume = mutableStateOf(150.toString()),
-                    waterAddition = mutableStateOf(20.toString())
+                    rotationLevel = mutableStateOf(
+                        (convertedData[5] * MultiplierRange.Germination.ROTATION_LEVEL + MinRangeValues.Germination.ROTATION_LEVEL)
+                            .toString()
+                    ),
+                    totalTime = mutableStateOf(
+                        (convertedData[6] * MultiplierRange.Germination.TOTAL_TIME + MinRangeValues.Germination.TOTAL_TIME)
+                            .toString()
+                    ),
+                    waterVolume = mutableStateOf(
+                        (convertedData[7] * MultiplierRange.Germination.WATER_VOLUME + MinRangeValues.Germination.WATER_VOLUME)
+                            .toString()
+                    ),
+                    waterAddition = mutableStateOf(
+                        (convertedData[8] * MultiplierRange.Germination.WATER_ADDITION + MinRangeValues.Germination.WATER_ADDITION)
+                            .toString()
+                    )
                 ),
                 kilning = KilningState(
-                    temperature = mutableStateOf(65.toString()),
-                    time = mutableStateOf(4.toString())
+                    temperature = mutableStateOf(
+                        (convertedData[9] * MultiplierRange.Kilning.TEMPERATURE + MinRangeValues.Kilning.TEMPERATURE)
+                            .toString()
+                    ),
+                    time = mutableStateOf(
+                        (convertedData[10] * MultiplierRange.Kilning.TIME + MinRangeValues.Kilning.TIME)
+                            .toString()
+                    )
                 )
             )
         }
